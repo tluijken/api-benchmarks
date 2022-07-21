e10a3bf5e7bcb26f755cdfb10b4b100771b692cd
Thomas Luijken <tluijken@outlook.com>
2022-07-21 22:09:37 +0200

    No automatic migrations for Rust....the database will be seeded with the db container start

--- rust/migrations/2022-07-15-124543_create_todos/up.sql
+++
@@ -1,6 +0,0 @@
--- Your SQL goes here
-CREATE TABLE todos (
-  id SERIAL PRIMARY KEY,
-  value TEXT NOT NULL,
-  checked BOOLEAN NOT NULL DEFAULT 'f'
-)
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,7 +1,5 @@
 #[macro_use]
 extern crate diesel;
-#[macro_use]
-extern crate diesel_migrations;
 
 use std::{error::Error, net::Ipv4Addr};
 
@@ -12,24 +10,14 @@ mod api;
 mod repository;
 mod schema;
 
-embed_migrations!();
 #[actix_web::main]
 async fn main() -> Result<(), impl Error> {
     env_logger::init();
 
-    let pool = repository::db_context::get_pool();
-    {
-        // This will run the necessary migrations.
-        match embedded_migrations::run(&pool.get().unwrap()) {
-            Ok(()) => println!("Succesfully applied pending migrations (if any)"),
-            Err(_) => println!("Unable to apply pending migrations"),
-        }
-    }
-    // Make instance variable of ApiDoc so all worker threads gets the same instance.
     HttpServer::new(move || {
         // This factory closure is called on each worker thread independently.
         App::new()
-            .app_data(Data::new(pool.clone()))
+            .app_data(Data::new(repository::db_context::get_pool()))
             .wrap(Logger::default())
             .configure(api::todo_controller::configure())
     })
