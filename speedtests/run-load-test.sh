0b11d4bb51e5659bcf4536a69612d29ec96a9ea6
Thomas Luijken <tluijken@outlook.com>
2022-08-03 15:49:07 +0200

    Isolated load tests

--- speedtests/run-load-test.sh
+++ speedtests/run-load-test.sh
@@ -2,7 +2,26 @@ docker-compose up -d influxdb grafana
 echo "--------------------------------------------------------------------------------------"
 echo "Load testing with Grafana dashboard http://localhost:3000/d/k6/k6-load-testing-results"
 echo "--------------------------------------------------------------------------------------"
+
+# make sure we start clean
+docker-compose --file ../docker-compose.yml down
+
+# start only the database and the todo api in go
+docker-compose --file ../docker-compose.yml up -d db todo_api_go
 docker-compose run --rm k6 run /scripts/go.js
+# cleanup after our test
+docker-compose --file ../docker-compose.yml down
+
+
+#repeat for all other tests
+docker-compose --file ../docker-compose.yml up -d db todo_api_rust
 docker-compose run --rm k6 run /scripts/rust.js
+docker-compose --file ../docker-compose.yml down
+
+docker-compose --file ../docker-compose.yml up -d db todo_api_node
 docker-compose run --rm k6 run /scripts/node.js
+docker-compose --file ../docker-compose.yml down
+
+docker-compose --file ../docker-compose.yml up -d db todo_api_dotnet
 docker-compose run --rm k6 run /scripts/dotnet.js
+docker-compose --file ../docker-compose.yml down
